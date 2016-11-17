dea908b2bdd37937829c5def2f2cd64d124e3539
Dipesh Dahal <dipesh@axle.com.np>
2016-11-17 17:26:16 +0545

    fixed contact

--- app/Contact.php
+++ app/Contact.php
@@ -6,20 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
-    protected $morphClass="Contact";
-    protected $table = 'contacts';
-    protected $fillable = [        
-        'name',        
-        'address',
-        'email',                
-        'website_link',
-        'latitude',
-        'longitude',
-        'social_media_id',
-    ];
 
-    public function contactable()
-    {
-        return $this->morphTo();
-    }
+
 }
