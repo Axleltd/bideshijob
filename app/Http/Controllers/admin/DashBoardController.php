b4f291ce1a26e4c7254cf3182f1f5d65ab4f5dee
kapil parajuli <kapilparajuli@kapils-MacBook-Pro.local>
2016-11-21 17:33:27 +0545

    added dash

---
+++ app/Http/Controllers/admin/DashBoardController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Company;
+use App\Training;
+
+class DashBoardController extends Controller
+{
+	protected $company;
+	protected $training;
+
+	public function __construct()
+	{
+		$this->company = new Company;
+		$this->training = new Training;
+	}
+
+    public function index()
+    {
+    	$company_count = $this->company->all()->count();
+    	$company_status = $this->company->where('status',1)->count();
+
+    	$training_count = $this->training->all()->count();
+    	$training_status = $this->training->where('featured',1)->count();
+
+
+    	
+    	return view('admin.index')->with([
+    		'company'=>$company_count,
+    		'training'=>$training_count]);
+    }
+}
