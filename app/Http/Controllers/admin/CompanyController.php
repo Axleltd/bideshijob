1a5c6814bc36694c705c2798486d4d6298ed2331
kapil parajuli <kapilparajuli@kapils-MacBook-Pro.local>
2016-11-22 14:36:17 +0545

    flash made

--- app/Http/Controllers/admin/CompanyController.php
+++ app/Http/Controllers/admin/CompanyController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Company;
 use Auth;
+use Session;
 use App\Notifications\BusinessNotification;
 class CompanyController extends Controller
 {
@@ -16,7 +17,7 @@ class CompanyController extends Controller
 	}
 	public function index()
 	{
-		$company = $this->company->all();
+		$company = $this->company->orderBy('created_at','DESC')->get();
 		return view('admin.company.index')->with('company',$company);
 	}
     public function active($companyId)
@@ -25,8 +26,11 @@ class CompanyController extends Controller
     	$active = $this->company->where('id',$companyId)->update(['status'=>1]);
     	 
     	if(!$active)
+    	{
+    		Session::flash('error', 'Company is unsuccessfully activated');
     		return redirect()->back();
-
+    	}
+    	Session::flash('success', 'Company is successfully activated');
     	Auth::user()->notify(new BusinessNotification('Congratulation Your Company '.$company->name.' is approved'));
     	return redirect('/dashboard/company');
 
@@ -37,7 +41,12 @@ class CompanyController extends Controller
 		$company = $this->company->where('id',$companyId)->first();
     	$active = $this->company->where('id',$companyId)->update(['status'=>0]);
     	if(!$active)
+    	{
+    		Session::flash('error', 'Company is unsuccessfully suspended');
     		return redirect()->back();
+    		
+    	}
+    	Session::flash('success', 'Company is successfully suspended');
     	Auth::user()->notify(new BusinessNotification('Sorry Your Company '.$company->name.' is Suspended'));
     	return redirect('/dashboard/company');    		
     }
@@ -47,7 +56,12 @@ class CompanyController extends Controller
     	$company = $this->company->where('id',$companyId)->first();
     	$destroy = $this->company->destroy('id',$companyId);
     	if(!$destroy)
+    	{
+    		Session::flash('error', 'Company is unsuccessfully deleted');
     		return redirect()->back();
+    		
+    	}
+    	Session::flash('success', 'Company is successfully deleted');
     	Auth::user()->notify(new BusinessNotification('Your Company '.$company->name.' is deleted'));
     	return redirect('dashboard/company');
     }
