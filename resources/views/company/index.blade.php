af21d7bc8c4aaa279c7446dafb987aa262ca77f4
zoro <itachi9841@gmail.com>
2016-11-24 14:08:37 +0545

    company page done '

--- resources/views/company/index.blade.php
+++ resources/views/company/index.blade.php
@@ -3,50 +3,58 @@
     Company
 @stop
 @section('content')
+
+<div class="page-inside">
 	
-	<div class="search">
-        {!! Form::open([
-                'action' => '\App\Http\Controllers\SearchController@companySearch','method'=>'get']) !!}
-		@include('company._search')
-        <button type="submit"  class="btn">Search</button>
-        {!! Form::close() !!}
-	</div>
-
-	<div class="section">
-		<h3>Explore Our Latest Company</h3>
-		<div class="section-content" id="ajax-form">
-            <ul class="archive-list">
-
-                @foreach ($company as $com)	                
-                    <li>
-                        <div class="wrap">
-                            <div class="img-wrap">
-                               <img src="{{asset('image/'.$com->logo)}}" alt="" width="300" height="300">
-                            </div>
-                            <div class="long-desc">
-                                <div class="row">
-                                    <h4 class="float-left">{{ $com->name }}</h4>
-                                    <div class="star float-right"></div>
-                                </div>
-                                <div class="row">
-                                    <p class="address">
-                                        @if($com->contacts)
-                                            {{ $com->contacts->address }}
-                                        @endif
-                                    </p>
-                                </div>                              
-                                <div class="row">
-                                    <p class="mb">{{ $com->description }}</p>                                            
-                                </div>
-                                <hr class="darker">
-                                <a href="{{ url('company/'.$com->id)}}" class="button">More Info</a>
-                            </div>
-                        </div>
-                    </li>
-                @endforeach
-            </ul>
-            {{ $company->links()}}
+	<div class="banner">
+    <div class="wrap">
+        
+        <div class="search">
+            
+            {!! Form::open([
+                    'action' => '\App\Http\Controllers\SearchController@companySearch','method'=>'get']) !!}
+            @include('company._search')
+            <button class="search-btn" type="submit"><i class="fa fa-search"></i></button>
+            {!! Form::close() !!}
         </div>
-	</div>
+    </div>
+    </div>
+    
+    <section class="page-content training">
+        
+        <div class="wrap row">
+          <div class="section-title">
+            <h3 class="wow fadeIn">All Agencies</h3>
+          </div>
+          
+            <div class="section-content">
+                <ul class="lists row">
+
+                        @foreach ($company as $com)	                
+                            <li class="col s12 m6 l4">
+                                <div class="wrap">
+                                    <div class="img-wrap">
+                                       <img src="{{asset('image/'.$com->logo)}}" alt="" width="300" height="300">
+                                    </div>
+                                    <div class="text-wrap">
+                                        <a class="float-left h5">{{ $com->name }}</a>
+                                        
+                                        <p class="address">
+                                            @if($com->contacts)
+                                                {{ $com->contacts->address }}
+                                            @endif
+                                        </p>
+                                       {{--  <p class="mb">{{ $com->description }}</p>                                       --}}      
+                                         <a href="{{ url('company/'.$com->id)}}" class="right">More Info</a>
+                                    </div>
+                                </div>
+                            </li>
+                        @endforeach
+                </ul>
+                {{ $company->links()}}
+            </div>
+    	</div>
+    </section>
+</div>
 
 @stop
