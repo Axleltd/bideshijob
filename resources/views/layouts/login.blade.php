5eabb7983e563ee12f4a2ea21c17396140ce30f4
parajulikapil <parajulikapil@gmail.com>
2016-11-25 12:47:10 +0545

    login done (#55)

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -2,23 +2,20 @@
 <head>
 	<title>Bideshi Job | @yield('title') </title>
 	<link rel="stylesheet" href="{{asset('css/materialize.css')}}">
+	<link rel="stylesheet" href="{{asset('stylesheets/screen.css')}}">
 	<script src="{{asset('js/jquery-3.1.0.js')}}"></script>
 	<script src="{{asset('js/materialize.js')}}"></script>
 </head>
 <body>
 
-	<div id="app">
+	<div id="app" class="login-page">
     
     <header class="header">
       <div class="logo-div">
-			<nav>
-			  <div class="nav-wrapper container">
-			      <a href="/"class=" center"><img src="{{asset('images/logo.png')}}" alt="" class="brand-logo" style="height:65px"></a>		    
-			  </div>
-			</nav>
+			<a href="/" class="center"><img src="{{asset('images/logo.png')}}" alt="" class="brand-logo" style="height:65px"></a>
 		</div>
 	</header>	
-	<div class="container">
+	<div class="">
 		@yield('content')	
 	</div>	
 		<div class="footer">
