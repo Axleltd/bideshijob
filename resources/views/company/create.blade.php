a5eb480c9731b894f35d824b0ce3ab51cd8064d9
zoro <itachi9841@gmail.com>
2016-11-25 18:40:22 +0545

    admin pannel served

--- resources/views/company/create.blade.php
+++ resources/views/company/create.blade.php
@@ -1,15 +1,33 @@
 @extends('layouts.dashboard')
 
 @section('content')
-	 {!! Form::open([
-                'action' => '\App\Http\Controllers\Company\CompanyController@store',
-                 'method'=>'post','files' => true]) !!}
-		@include('company._form')
-		@include('company._contact')
-		
-		
-		<button type="submit" class="waves-effect waves-light btn">Continue</button>
-	{!! Form::close() !!}
+
+
+	<div class="section-title">
+		<h3>Add a Agency</h3>
+		<ul class="bread-crumb">
+			<li><a href="">Dashboard</a></li>/
+			<li><a href="">Company Name</a></li>/
+			<li><a href="#">Create Agency</a></li>
+
+		</ul>
+	</div>
+
+	<div class="section-content">
+		<h5>Agency Details</h5>
+
+		<div class="row">
+			 {!! Form::open([
+		                'action' => '\App\Http\Controllers\Company\CompanyController@store',
+		                 'method'=>'post','files' => true]) !!}
+				@include('company._form')
+				@include('company._contact')
+				
+				
+				<button type="submit" class="waves-effect waves-light btn">Continue</button>
+			{!! Form::close() !!}
+		</div>
+	</div>
 @stop
 
 
--- resources/views/job/create.blade.php
+++ resources/views/job/create.blade.php
@@ -2,12 +2,28 @@
 
 @section('content')
 
-<h3>Add New Job</h3>
-	 {!! Form::open([
-        'action' => ['\App\Http\Controllers\JobsController@store',$id],
-        'method'=>'post','files' => true]) !!}
-		@include('job._form')
-		<button type="submit" class="waves-effect waves-light btn">Continue</button>
-	{!! Form::close() !!}
+
+	<div class="section-title">
+		<h3>Add a Job</h3>
+		<ul class="bread-crumb">
+			<li><a href="">Dashboard</a></li>/
+			<li><a href="">Company Name</a></li>/
+			<li><a href="#">Create Job</a></li>
+
+		</ul>
+	</div>
+
+	<div class="section-content">
+		<h5>Job Details</h5>
+
+		<div class="row">
+			 {!! Form::open([
+		        'action' => ['\App\Http\Controllers\JobsController@store',$id],
+		        'method'=>'post','files' => true]) !!}
+				@include('job._form')
+				<button type="submit" class="waves-effect waves-light btn">Continue</button>
+			{!! Form::close() !!}
+		</div>
+	</div>
 
 @stop
