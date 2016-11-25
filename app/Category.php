e3c83110a3a083dd2dcc5bb774d74f6f6d391bc6
Ramesh <rpaudel58@gmail.com>
2016-11-25 16:50:39 +0545

    sluggable blog

--- app/Category.php
+++ app/Category.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Category extends Model
 {
+	use HasSlug;
 	protected $fillable = [
 		'name',
 		'slug',
@@ -14,4 +17,11 @@ class Category extends Model
 	];
 
 	protected $casts = ['featured' => 'boolean','order' => 'integer'];
+
+	public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
 }
