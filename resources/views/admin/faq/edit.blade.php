14fccbed5dc0306214776666a7b4a32a0689efb3
zoro <itachi9841@gmail.com>
2016-11-25 19:44:02 +0545

    edit pages coverd under Edit

--- resources/views/admin/faq/edit.blade.php
+++ resources/views/admin/faq/edit.blade.php
@@ -1,13 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
+	<div class="section-title">
+		<h3>Edit FAQ</h3>
+		<ul class="bread-crumb">
+			<li><a href="">Dashboard</a></li>/
+			{{-- <li><a href="">Company Name</a></li>/ --}}
+			<li><a href="#">Edit FAQ</a></li>
 
-<h3>Edit New FAQ</h3>
-	 {!! Form::model($faq,[
-        'action' => ['\App\Http\Controllers\FAQController@update',$faq->id],
-        'method'=>'put']) !!}
-		@include('admin.faq._form')
-		<button type="submit" class="waves-effect waves-light btn">Continue</button>
-	{!! Form::close() !!}
+		</ul>
+	</div>
+
+	<div class="section-content">
+		<h5>FAQ Details</h5>
+
+		<div class="row">
+			 {!! Form::model($faq,[
+		        'action' => ['\App\Http\Controllers\FAQController@update',$faq->id],
+		        'method'=>'put']) !!}
+				@include('admin.faq._form')
+				<button type="submit" class="waves-effect waves-light btn">Continue</button>
+			{!! Form::close() !!}
+		</div>
+	</div>
 
 @stop
