e42829192fbbd9f29f6765d87202eb2049f836a5
parajulikapil <parajulikapil@gmail.com>
2016-11-27 09:36:55 +0545

    dashboard edit and add some security in training and company (#71)

--- resources/views/admin/company/index.blade.php
+++ resources/views/admin/company/index.blade.php
@@ -7,8 +7,12 @@
 			<a href="{{url('/company/'.$com->id)}}"><h3>{{$com->name}}</h3></a>
 			<img src="{{asset('image/'.$com->logo)}}" alt="">
 			<a href="{{ url('dashboard/company/active/'.$com->id)}}" class="{{ ($com->status == 1) ?'disabled': null }} btn">Active</a>
-			<a href="{{ url('dashboard/company/suspend/'.$com->id)}}" class="{{ ($com->status == 0) ?'disabled': null }} btn">Suspend</a>
-			<a href="{{ url('dashboard/company/delete/'.$com->id)}}" class="btn">Delete</a>
+			<a href="{{ url('dashboard/company/suspend/'.$com->id)}}" class="{{ ($com->status == 0) ?'disabled': null }} btn">Suspend</a>			
+			{!! Form::model($com,[
+		                'action' => ['\App\Http\Controllers\admin\CompanyController@destroy',$com->id],'method'=>'delete']) !!}				
+				<button type="submit" class="waves-effect waves-light btn">Delete</button>
+		    {!! Form::close() !!}
+			
 
 		@endforeach
 	</div>
