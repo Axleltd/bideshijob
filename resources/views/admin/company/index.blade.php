93bbbb0c18a95ee542e4b0d2361f9b9ecca58486
parajulikapil <parajulikapil@gmail.com>
2016-11-27 18:11:52 +0545

    profile push (#79)

--- resources/views/admin/company/index.blade.php
+++ resources/views/admin/company/index.blade.php
@@ -1,10 +1,10 @@
-@extends('layouts.app')
+@extends('layouts.dashboard')
 @section('content')
 	<div class="company">
 		@include('admin._flash')
 		@foreach($company as $com)
 			
-			<a href="{{url('/company/'.$com->id)}}"><h3>{{$com->name}}</h3></a>
+			<a target="_blank" href="{{url('/company/'.$com->id)}}"><h3>{{$com->name}}</h3></a>
 			<img src="{{asset('image/'.$com->logo)}}" alt="">
 			<a href="{{ url('dashboard/company/active/'.$com->id)}}" class="{{ ($com->status == 1) ?'disabled': null }} btn">Active</a>
 			<a href="{{ url('dashboard/company/suspend/'.$com->id)}}" class="{{ ($com->status == 0) ?'disabled': null }} btn">Suspend</a>			
