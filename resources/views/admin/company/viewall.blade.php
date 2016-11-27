e27fd8c62f62d523737676ca1c039629c7dbb58f
parajulikapil <parajulikapil@gmail.com>
2016-11-27 18:33:30 +0545

    made some changes in profile (#80)

--- resources/views/admin/company/viewall.blade.php
+++ resources/views/admin/company/viewall.blade.php
@@ -5,18 +5,35 @@
 @stop
 
 @section('content')
+		
+	<div class="section-title">
+		<h3>All Company</h3>
+		<ul class="bread-crumb">
+			<li><a href="">Dashboard</a></li>/
+			<li><a href="">Blog</a></li>/
+			<li><a href="#">Category</a></li>
+
+		</ul>
+	</div>
 
 	<div class="company">
-		<ul>
-			@foreach($companies as $company)
-				
-				<li>
-					<img src="{{asset('image/'.$company->logo)}}" alt="">
-					{{$company->name}}
-				</li>
-		
-			@endforeach
-		</ul>
+		@if(!count($companies)>0)
+				<h5>You have not created a company yet!</h5>
+				<a class="btn" href="{{url('/company/create')}}">Create A Company</a>
+		@else
+			<ul>
+				@foreach($companies as $company)
+					
+					<li>
+						<img src="{{asset('image/'.$company->logo)}}" alt="">
+						<h5>{{$company->name}}</h5>
+						<a href="{{url('/company/'.$company->id.'/job/create')}}" class="btn">Create a new job</a>
+						<a href="{{url('/company/'.$company->id.'/training/create')}}" class="btn">Create a new training</a>
+					</li>
+			
+				@endforeach
+			</ul>
+		@endif			
 	</div>
 
 @stop
