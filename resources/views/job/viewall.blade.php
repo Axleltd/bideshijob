e85b0103bed37e1177537677d6019967b8bffbad
prashant9841 <itachi9841@gmail.com>
2016-11-28 17:41:30 +0545

    Create viewall.blade.php

---
+++ resources/views/job/viewall.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.dashboard')
+
+@section('title')
+	Job
+@stop
+
+@section('content')
+
+	<div class="section-title">
+		<h3>All jobs</h3>
+		<ul class="bread-crumb">
+			<li><a href="">Dashboard</a></li>/
+			<li><a href="">Blog</a></li>/
+			<li><a href="#">Category</a></li>
+
+		</ul>
+	</div>
+
+	<div class="section-content">		
+		
+		<div class="row">
+			@if(!count($jobs)>0)
+				<h5>You have not created a job yet!</h5>
+				<a class="btn" href="{{url('/profile/company/')}}">Create A New Job</a>
+			@else
+			<ul>
+				@foreach($jobs as $job)
+					
+					<li class="col s12 m4">
+						<div class="card">
+							<div class="card-image">
+								<img src="{{asset('image/'.$job->company->logo)}}" alt="">
+							</div>
+							{{-- <div class="card-content"> --}}
+								<span class="card-title"><a href="{{url('/company/'.$job->company->id.'/job/'.$job->id)}}">{{$job->title}}</a></span>
+								
+							{{-- </div> --}}
+								
+						</div>
+					</li>
+			
+				@endforeach			
+			</ul>
+		@endif
+		</div>	
+		
+	</div>
+
+@stop
