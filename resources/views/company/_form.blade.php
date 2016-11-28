8695779eb32061d2986b5d4b01779b2c96a185ec
zoro <itachi9841@gmail.com>
2016-11-28 17:04:37 +0545

    company create

--- resources/views/company/_form.blade.php
+++ resources/views/company/_form.blade.php
@@ -14,23 +14,10 @@
 		    @endif		
 		</div>
 		<div class="col s12 m6">
-			{!! Form::label('description','Description:') !!}
-			{!! Form::textarea('description',old('description')) !!}
-			@if(count($errors->get('description')) > 0)
-		        <div class="alert alert-danger">
-		            <ul>
-		                @foreach($errors->get('description') as $error)
-		                    <li>{{ $error  }}</li>
-		                @endforeach
-		            </ul>
-		        </div>
-		    @endif		
-		</div>
-		<div class="form-group">
 
-		   		{!! Form::label('logo', 'Logo:', ['class' => 'control-label']) !!}
+		   	{!! Form::label('logo', 'Logo:', ['class' => 'control-label']) !!}
 		   		
-		</div>
+		
 
 
 		    @if(isset($company->logo))
@@ -52,8 +39,8 @@
 
 		        {!! Form::file('logo', array('onchange'=>'loadFile(event)','id'=>'image')) !!}
 		       </div>
-		      </div>
-		        @if(count($errors->get('logo')) > 0)
+		    </div>
+		    @if(count($errors->get('logo')) > 0)
 		        <div class="alert alert-danger">
 		            <ul>
 		                @foreach($errors->get('logo') as $error)
@@ -63,7 +50,21 @@
 		        </div>
 		    @endif		
 
-	</div>
+		</div>
+
+		<div class="col s12">
+			{!! Form::label('description','Description:') !!}
+			{!! Form::textarea('description',old('description')) !!}
+			@if(count($errors->get('description')) > 0)
+		        <div class="alert alert-danger">
+		            <ul>
+		                @foreach($errors->get('description') as $error)
+		                    <li>{{ $error  }}</li>
+		                @endforeach
+		            </ul>
+		        </div>
+		    @endif		
+		</div>
 	</div>
 </div>
 
