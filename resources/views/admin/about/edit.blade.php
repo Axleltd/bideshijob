89c045540391df24ddf4ea3b2adff023142a1228
parajulikapil <parajulikapil@gmail.com>
2016-11-30 17:51:49 +0545

    about page (#112)

--- resources/views/admin/about/edit.blade.php
+++ resources/views/admin/about/edit.blade.php
@@ -16,7 +16,7 @@
 		<h5>About Us Page Content</h5>
 		@include('admin._flash')
 		<div class="row">
-			 {!! Form::model($about, ['action'=>['\App\Http\Controllers\admin\AboutUsController@update',$about->id],'method'=>'PUT']) !!}
+			 {!! Form::model($about, ['action'=>['\App\Http\Controllers\admin\AboutUsController@update',$about->id],'method'=>'PUT','files' => true]) !!}
 				@include('admin.about._form')			
 				<button type="submit" class="waves-effect waves-light btn">Continue</button>
 			{!! Form::close() !!}
--- resources/views/site/about.blade.php
+++ resources/views/site/about.blade.php
@@ -11,10 +11,13 @@
 			<div class="wrap row">
 				<div class="section-title">
 					<h3 class="wow fadeIn">@lang('site.about')</h3>
-				</div>
-				<div class="section-content">
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui eveniet, molestiae a quae laborum praesentium, quod reprehenderit sed molestias mollitia iste repellendus, perferendis in unde beatae deleniti ipsum maiores incidunt at quia et asperiores! Harum blanditiis iusto eligendi, vel reprehenderit tempora, molestias quas quasi nisi, a ex provident quis, voluptatibus assumenda nobis tempore magni illo modi! Natus molestiae ut dolorem, labore adipisci vel ipsa maiores, veritatis deserunt consequatur earum. Consectetur, ratione dolorem aspernatur. Reprehenderit asperiores est, nesciunt architecto molestiae quos quas, nemo, explicabo dicta laudantium similique cumque repudiandae eius sit fugiat, necessitatibus quibusdam at dolor aliquid. A sapiente ipsam, architecto.</p>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui eveniet, molestiae a quae laborum praesentium, quod reprehenderit sed molestias mollitia iste repellendus, perferendis in unde beatae deleniti ipsum maiores incidunt at quia et asperiores! Harum blanditiis iusto eligendi, vel reprehenderit tempora, molestias quas quasi nisi, a ex provident quis, voluptatibus assumenda nobis tempore magni illo modi! Natus molestiae ut dolorem, labore adipisci vel ipsa maiores, veritatis deserunt consequatur earum. Consectetur, ratione dolorem aspernatur. Reprehenderit asperiores est, nesciunt architecto molestiae quos quas, nemo, explicabo dicta laudantium similique cumque repudiandae eius sit fugiat, necessitatibus quibusdam at dolor aliquid. A sapiente ipsam, architecto.</p>
+				</div>				
+				<div class="section-content">				
+					@if($about)
+						<img src="{{asset('image/about/'.$about->image)}}" alt="" width="300" height="300">
+						<p>{!!$about->content!!}</p>
+					@endif					
+					
 				</div>
 			</div>
 		</section>
