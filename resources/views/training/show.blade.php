f9ce9ff881c1e6bc036a1b318c30f8d896ef9b9e
Dipesh Dahal <dipesh@axle.com.np>
2016-12-07 16:53:46 +0545

    inside jobs done

--- resources/views/training/show.blade.php
+++ resources/views/training/show.blade.php
@@ -1,54 +1,49 @@
 @extends('layouts.app')
     
 @section('content')
-	 <div class="page-inside page-single">
+    <div class="page-inside page-single">
+        <div class="banner">
+            <div class="wrap">
+                <h3>{{ $job->title }}</h3>
+
+                <ul class="social">
+                  <li><a href="https://facebook/itachi9841"><i class="fa fa-facebook"></i></a></li>
+                  <li><a href="https://twitter/itachi9841"><i class="fa fa-twitter"></i></a></li>
+                </ul>
+            </div>
+
+        </div>
          <section>
              
             <div class="wrap row">
-                <div class="section-title row">                    
-                    <h3 class="col s6">{{ $training->title }}</h3>
-                    <p class="social col s6" style="text-align: center;">
-                      <a href="#"><i class="ti-facebook"></i></a> <a href="#"><i class="ti-twitter"></i></a> <a href="#"><i class="ti-googleplus"></i></a>
-                    </p>
-                </div>            
+                       
                 <div class="section-content">
                     <div class="row">
-                        <div class="s12 m8 col"> 
+                        <div class="s12 m6 col"> 
                             <img src="{{asset('image/training/'.$training->image)}}" alt="">                           
+                            
+
+                        </div>
+                        <div class="s12 m6 col">
+
                             <p>Country: {{$training->country}}</p>                
                             <p>Quantity: {{$training->quantity}}</p>                                                                        
                             <p>Training Time: {{$training->from}}-{{$training->to}}</p>
                             <p>Fees: {{$training->fees}}</p>    
                             <p>Categories: {{$training->categories}}</p>                            
-
-                        </div>
-                        <div class="s12 m4 col">
-                            <div class="company-wrap row">
-                                <div class="img-wrap">
-                                    <img src="{{asset('image/'.$training->company->logo)}}" alt="">
-                                </div>
-                                <div class="text-wrap">
-                                    <h5><a href="#">{{$training->company->name}}</a></h5>
-                                    <p>Address: {{$training->company->contacts->address}}</p>
-                                    <a href="{{ url('/company/'.$training->company->slug)}}" class="right">View more</a>
-                                </div>  
-                            </div>
                         </div>
                     </div>
-                    <hr>
-                    <div class="row">
+                    <div class="row description">
                         <p>Description: {{$training->description}}</p>
 
 
                     </div>
-                    <div class="row">
-                        <div class="col s12 m6">
-                            
-                        </div>
-                        <div class="col s12 m6">
-                            <button class="btn waves-effect">Apply Now</button>
-                        </div>
+                    <div class="row btn-row">
+                        <button class="btn waves-effect">Apply Now</button>
                     </div>
+
+
+                    <hr>
                                   
                 </div>
 
